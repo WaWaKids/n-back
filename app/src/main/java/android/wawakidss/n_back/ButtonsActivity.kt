e8c4e1590574eb1296bf402ac1ddc4aec25b724b
Максим <timaltair@yandex.ru>
2020-12-16 20:24:35 +0300

    -The logic works perfect now. Only need to understand whats happening with button colors

--- app/src/main/java/android/wawakidss/n_back/ButtonsActivity.kt
+++ app/src/main/java/android/wawakidss/n_back/ButtonsActivity.kt
@@ -1,42 +1,45 @@
 package android.wawakidss.n_back
 
 import android.content.Intent
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 import java.util.concurrent.TimeUnit
-import java.util.ArrayDeque
 
 class ButtonsActivity : AppCompatActivity(), View.OnClickListener {
 
     val n = 1
     var score = 0
     val TAG: String = "ButtonsActivity"
-    var temp = ArrayDeque<Int>(n)
+    var temp = ArrayDeque<ImageButton>(n)
     lateinit var text: TextView
-    lateinit var btn: Array<ImageView>
+    lateinit var btn: Array<ImageButton>
     lateinit var menuButton: Button
+    val KEY_INDEX = "index"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.buttons_activity)
 
+        if (savedInstanceState != null)
+            score = savedInstanceState.getInt(KEY_INDEX, 0);
+
         text = findViewById<TextView>(R.id.score)
         btn = arrayOf(
-            findViewById<ImageView>(R.id.btn11),
-            findViewById<ImageView>(R.id.btn12),
-            findViewById<ImageView>(R.id.btn13),
-            findViewById<ImageView>(R.id.btn21),
-            findViewById<ImageView>(R.id.btn22),
-            findViewById<ImageView>(R.id.btn23),
-            findViewById<ImageView>(R.id.btn31),
-            findViewById<ImageView>(R.id.btn32),
-            findViewById<ImageView>(R.id.btn33))
+            findViewById<ImageButton>(R.id.btn11),
+            findViewById<ImageButton>(R.id.btn12),
+            findViewById<ImageButton>(R.id.btn13),
+            findViewById<ImageButton>(R.id.btn21),
+            findViewById<ImageButton>(R.id.btn22),
+            findViewById<ImageButton>(R.id.btn23),
+            findViewById<ImageButton>(R.id.btn31),
+            findViewById<ImageButton>(R.id.btn32),
+            findViewById<ImageButton>(R.id.btn33))
 
         text.setText(Integer.toString(score))
 
@@ -51,24 +54,32 @@ class ButtonsActivity : AppCompatActivity(), View.OnClickListener {
             btn[i].setOnClickListener(this)
         }
 
-        for (i in 0..n-1) {
-            temp.push(btn[(0..8).shuffled().first()].getId())
-            
+        for (i in 0..n) {
+            temp.addLast(btn[(0..8).shuffled().first()])
         }
     }
 
     override fun onClick(view: View) {
-        Log.d(TAG, "user pressed " + Integer.toString(view.getId()))
-        Log.d(TAG, "the right button was " + Integer.toString(temp.peekLast()))
-        if (view.getId() == temp.pollLast()) {
+        val v: ImageButton = view as ImageButton
+        Log.d(TAG, "user pressed " + Integer.toString(v.getId()))
+        Log.d(TAG, "the right button_grey was " + Integer.toString(temp.peekLast().getId()))
+
+        if (v.getId() == temp.pollLast().getId()) {
             score += 1
         }
+
         else {
             score = 0
-            view.setBackgroundColor(Color.RED)
         }
-        TimeUnit.MILLISECONDS.sleep(500)
         text.setText(Integer.toString(score))
-        temp.push(btn[(0..8).shuffled().first()].getId())
+        temp.peekFirst().setColorFilter(R.color.colorGrey)
+        temp.addFirst(btn[(0..8).shuffled().first()])
+        temp.peekFirst().setColorFilter(R.color.colorGrey)
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSavedInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, score)
     }
 }
