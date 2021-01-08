954044325540e8fc5229b135859cef50ec025a71
WaWaKids <wawakidss@gmail.com>
2021-01-09 02:57:08 +0300

    -Added seekBar so now you can choose 'n'-number

--- app/src/main/java/android/wawakidss/n_back/MenuActivity.kt
+++ app/src/main/java/android/wawakidss/n_back/MenuActivity.kt
@@ -4,20 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
 
+    private lateinit var n: TextView
+    private lateinit var seekBar: SeekBar
+    private lateinit var btn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu_activity)
 
-        val btn = findViewById<Button>(R.id.start_button)
+        n = findViewById<TextView>(R.id.text_n)
+        seekBar = findViewById<SeekBar>(R.id.seekBar)
+        btn = findViewById<Button>(R.id.start_button)
+
+        n.setText("n = 1")
+
+        seekBar.setOnSeekBarChangeListener(this)
 
         btn.setOnClickListener {
-            val intent = Intent(this, ButtonsActivity::class.java)
-
+            var intent = Intent(this, ButtonsActivity::class.java)
+            intent.putExtra("n", seekBar.progress)
             startActivity(intent)
         }
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+            n.setText("n = " + seekBar?.progress.toString() )
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
     }
 }
